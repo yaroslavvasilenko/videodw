abaf46a6d801b6b705140f2a34a28e58435e4c5f
Yaroslav_Vasilenko <yaroslav.vasilenko3101@gmail.com>
2022-03-19 14:03:12 +0300

    Update dw.go
    Добавил загрузку нескольких файлов одновременно

--- src/dw/dw.go
+++ src/dw/dw.go
@@ -2,71 +2,72 @@ package dw
 
 import (
 	"fmt"
-	"github.com/schollz/progressbar/v3"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
+	"time"
 	"video-dw/src/handler-err"
 )
 
 func DownloadFile(filepath string, urls []string) {
+	wg := new(sync.WaitGroup)
 	var nameFile int
 	for _, url := range urls {
 		nameFile++
 		log.Printf("STARTED DOWNLOADING URL: %s\n", url)
-		err := downloadSingleFile(url, filepath+strconv.Itoa(nameFile)+".mp4")
+		wg.Add(1)
+		err := downloadSingleFile(url, filepath+strconv.Itoa(nameFile)+".mp4", wg)
 		if err != nil {
 			log.Println(err)
-			// ToDo: add comments to the error
-			// e.g. "Problem with download has occurred"
-			// count append name video
-			// оставать имя из json (используй unicode decoder!!!)
 		}
+		time.Sleep(time.Second * 3)
 	}
+	wg.Wait()
 }
 
-func downloadSingleFile(url string, filepath string) *handler_err.DownloadError {
-	out, err := os.Create(filepath)
-	if err != nil {
-		return &handler_err.DownloadError{
-			InterruptedDownload: url,
-			Message:             fmt.Sprintf("Failed to create file: %s", filepath),
-			Err:                 err}
-	}
-	defer out.Close()
+func downloadSingleFile(url string, filepath string, wg *sync.WaitGroup) *handler_err.DownloadError {
+	go func() *handler_err.DownloadError {
+		out, err := os.Create(filepath)
+		if err != nil {
+			return &handler_err.DownloadError{
+				InterruptedDownload: url,
+				Message:             fmt.Sprintf("Failed to create file: %s", filepath),
+				Err:                 err}
+		}
+		defer out.Close()
 
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return &handler_err.DownloadError{
-			InterruptedDownload: url,
-			Message:             "Get request failed",
-			Err:                 err}
-	}
-	defer resp.Body.Close()
+		// Get the data
+		resp, err := http.Get(url)
+		if err != nil {
+			return &handler_err.DownloadError{
+				InterruptedDownload: url,
+				Message:             "Get request failed",
+				Err:                 err}
+		}
+		defer resp.Body.Close()
 
-	// Check server response
-	if resp.StatusCode != http.StatusOK {
-		return &handler_err.DownloadError{
-			InterruptedDownload: url,
-			Message:             "Status code doesn't equal 200",
-			Err:                 err}
-	}
+		// Check server response
+		if resp.StatusCode != http.StatusOK {
+			return &handler_err.DownloadError{
+				InterruptedDownload: url,
+				Message:             "Status code doesn't equal 200",
+				Err:                 err}
+		}
 
-	bar := progressbar.DefaultBytes(
-		resp.ContentLength,
-		filepath,
-	)
-
-	// Writer the body to file
-	_, err = io.Copy(io.MultiWriter(out, bar), resp.Body)
-	if err != nil {
-		return &handler_err.DownloadError{
-			InterruptedDownload: url,
-			Message:             "Failed to write data",
-			Err:                 err}
-	}
+		// Writer the body to file
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			return &handler_err.DownloadError{
+				InterruptedDownload: url,
+				Message:             "Failed to write data",
+				Err:                 err}
+		}
+		wg.Done()
+		log.Println("Один файл загружен")
+		return nil
+	}()
 	return nil
 }
